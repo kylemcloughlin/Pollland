65028fcc3a5db2feaf81c6aaca06c535d36a9508
weihansu <weihan.sw@gmail.com>
2019-03-01 21:05:49 +0000

    add database helpers

--- express-server-test.js
+++ express-server-test.js
@@ -30,23 +30,60 @@ app.set('view engine', 'ejs');
  */
 
 // HomePage
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.post("/getEmail", (req, res) => {
+app.post('/getEmail', (req, res) => {
     const email = req.body.email;
 
-    poll.saveEmail(email)
-        .then( (result) => {
-            res.json(result); // redirect to create poll page
+    // poll.saveEmail(email)
+    //     .then( () => {
+    //         poll.getVoterBy('email', email)
+    //             .then((result) => {
+    //                 // obj with voter filter by email
+    //                 let voterRow = result;
+    //                 // redirect to create poll
+    //                 res.json(voterRow);
+    //             })
+    //     })
+    valueObj = {
+        email: email,
+        encrypted_id: poll.generateRandomString(6)
+    }
+    poll.insertToDatabase('voter', valueObj)
+        .then( () => {
+            poll.getVoterBy('email', email)
+                .then((result) => {
+                    // obj with voter filter by email
+                    let voterRow = result;
+                    // redirect to create poll
+                    res.json(voterRow);
+                })
         })
 });
 
 
 
 // Create Poll
+app.get('/create/:voterID', (req, res) => {
+    let voterID = req.params.voterID;
 
+    poll.getVoterBy('id', voterID)
+        .then( (result) => {
+            let voterRow = result;
+            res.json(voterRow);
+        })
+});
+
+app.post('/HERE_YOUR_POST_FORM', (req, res) => {
+    let table;
+    let valueObj;
+    poll.insertToDatabase(table, valueObj)
+        .then( () => {
+            res.redirect()
+        })
+})
 
 
 // Poll Confirm
@@ -58,6 +95,14 @@ app.post("/getEmail", (req, res) => {
 
 
 // Poll Results
+app.get('/poll/:questionID/result', (req, res) => {
+    let questionID = req.params.questionID;
+
+    poll.getPoints(questionID)
+        .then( (result) => {
+            res.json(result)
+        })
+})
 
 
 
