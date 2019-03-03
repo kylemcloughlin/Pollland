2902907b47c85818dbbe80e877da0f8e9c546495
LaviIonas <laviionas772@gmail.com>
2019-03-03 17:35:48 +0000

    changed rank page to accept array from db

--- public/js/ranking_drag.js
+++ public/js/ranking_drag.js
@@ -4,14 +4,17 @@ console.log("js file opened");
 
 // const numberOfOptions = 5;
 
+const optionsArr = ["Meatballs", "Spaghetti", "Raveoili", "Cancer"];
+
+//Take in an array that will be displayed
 // const optionsArr = getOptions(url.params.questionID);
 
 
 
 var createOptions = function(optionsArr) {
-    for (var x = 1; x < optionsArr.length + 1; x++) {
-        var li = $('<li>' + optionsArr[x].text + '</li>');
-        var rank = $('<li>' + optionsArr[x].id + '</li>');
+    for (var x = 0; x < optionsArr.length; x++) {
+        var li = $('<li>' + optionsArr[x] + '</li>');
+        var rank = $('<li>' + (x+1) + '</li>');
         $('ul.wishlist').append(li);
         $('.rank').append(rank);
     }
@@ -33,7 +36,7 @@ app.init = function() {
             arr1.push(arrLi1[i].innerHTML);
         }
         alert("submitted");
-        $.post("/poll/pollID/rank", {array: arr1});
+        $.post("/poll/pollID/voterID/rank", {array: arr1});
     };
 };
 $(function() {
@@ -41,5 +44,5 @@ $(function() {
 });
 $(document).ready(() => {
     // renderOptions(data);
-    createOptions(data);
+    createOptions(optionsArr);
 });
